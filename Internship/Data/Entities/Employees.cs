b0c2fd5d69428f57b290beb41488d6e261408422
seifeldinabdullah <seifeldin.heshaam@gmail.com>
2023-09-11 14:47:14 +0300

    relation

--- Internship/Data/Entities/Employees.cs
+++ Internship/Data/Entities/Employees.cs
@@ -1,15 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Internship.Data.Entities
 {
     public class Employees
     {
         public Guid Id { get; set; }
-        public required Department DepId { get; set; }
+        
         public string? Name { get; set; }
         public int Age  { get; set; }
         public string? Mail { get; set; }   
         public string? Position { get; set; }
         public double? Salary { get; set; }
+
+
+        public Guid DeptId { get; set; }
+
+        [ForeignKey(nameof(DeptId))]
+        public Department Departments { get; set; }
     }
 }
