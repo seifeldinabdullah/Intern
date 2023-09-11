34ea4df16d61ed2e722dba503087b93ec491dbd5
seifeldinabdullah <seifeldin.heshaam@gmail.com>
2023-09-11 11:58:02 +0300

    Add project files.

---
+++ Internship/Data/Entities/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace Internship.Data.Entities
+{
+    public class Department
+    {
+        public Guid DepId { get; set; }
+        public string? DepName { get; set; }
+        public string? Office { get; set; }
+
+    }
+}
---
+++ Internship/Data/Entities/Employees.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Internship.Data.Entities
+{
+    public class Employees
+    {
+        public Guid Id { get; set; }
+        public required Department DepId { get; set; }
+        public string? Name { get; set; }
+        public int Age  { get; set; }
+        public string? Mail { get; set; }   
+        public string? Position { get; set; }
+        public double? Salary { get; set; }
+    }
+}
